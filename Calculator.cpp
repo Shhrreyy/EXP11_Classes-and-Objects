492729a0f2c6ec58a51bf4f3349d117215cee418
Shreyy <shriyanshyadav2006@gmail.com>
2025-08-22 10:22:30 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+private:
+    double num1, num2;
+
+public:
+    Calculator(double a, double b) {
+        num1 = a;
+        num2 = b;
+    }
+    double add() {
+        return num1 + num2;
+    }
+    double subtract() {
+        return num1 - num2;
+    }
+    double multiply();
+    double divide();
+};
+double Calculator::multiply() {
+    return num1 * num2;
+}
+double Calculator::divide() {
+    if (num2 != 0)
+        return num1 / num2;
+    else {
+        cout << "Error:" << endl;
+        return 0;
+    }
+}
+
+int main() {
+    double a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    Calculator calc(a, b);
+
+    cout << "Addition: " << calc.add() << endl;
+    cout << "Subtraction: " << calc.subtract() << endl;
+    cout << "Multiplication: " << calc.multiply() << endl;
+    cout << "Division: " << calc.divide() << endl;
+
+    return 0;
+}
