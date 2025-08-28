2bc0bf81637b54856c9edbac2cfbc4210bee7c24
Shreyy <shriyanshyadav2006@gmail.com>
2025-08-28 16:01:50 +0530

    Create cubevolume_publicpvt.cpp

---
+++ cubevolume_publicpvt.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Cube {
+private:
+    double side;
+public:
+    Cube(double s) {
+        side = s;
+    }
+
+    double volume() {
+        return side * side * side;
+    }
+
+    double getSide() {
+        return side;
+    }
+};
+
+double calculateVolume(Cube &c) {
+    double side = c.getSide();
+    return side * side * side;
+}
+
+int main() {
+    Cube c1(3);
+    cout << "Volume of the cube (inside class): " << c1.volume() << endl;
+    cout << "Volume of the cube (outside class): " << calculateVolume(c1) << endl;
+    return 0;
+}
